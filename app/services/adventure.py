e0c84165890c7ac423be24a97aeea8923a9ea5f6
Tal Benadon <talbenadon2@gmail.com>
2025-04-29 14:25:37 +0900

    adventure status and type

---
+++ app/services/adventure.py
@@ -0,0 +1,9 @@
+class Adventure:
+    def __init__(self, type):
+        #should generate some starting story and decisions when initialized with start
+        self.status = "start" #maybe add an additional "starting info" to let the ai generate a different type of start?
+
+        self.type = type # type of the adventure (slice of life, fantasy, etc..)
+    
+
+    
