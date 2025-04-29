e9f0a465697c37458c6fcdabdb7452d9aae313c2
Tal Benadon <talbenadon2@gmail.com>
2025-04-29 14:34:18 +0900

    simple is starting scene tool created

--- app/services/adventure.py
+++ app/services/adventure.py
@@ -1,9 +1,16 @@
 class Adventure:
-    def __init__(self, type):
+    def __init__(self, type : str = "fantasy"):
         #should generate some starting story and decisions when initialized with start
-        self.status = "start" #maybe add an additional "starting info" to let the ai generate a different type of start?
+        self.status = 0 #maybe add an additional "starting info" to let the ai generate a different type of start?
+        # maybe change status to numbers? 0 is start, and the story progresses by numbers of "scenes" generated
 
         self.type = type # type of the adventure (slice of life, fantasy, etc..)
+
+    def is_starting_scene(self):
+        return self.status == 0
+
     
+    
+
 
     
