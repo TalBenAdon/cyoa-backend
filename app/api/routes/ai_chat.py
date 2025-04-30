299ff292ab3708eafee8474390f688ea5c0cbfcc
Tal Benadon <talbenadon2@gmail.com>
2025-04-30 21:58:05 +0900

    changed openrouterclient into a class

--- app/api/routes/ai_chat.py
+++ app/api/routes/ai_chat.py
@@ -1,18 +1,19 @@
 from fastapi import APIRouter, HTTPException
-from app.services.openrouter_client import chat_with_ai
+from app.services.openrouter_client import OpenRouterClient
 from app.models.ai import PromptRequest
 from app.core.logger import get_logger
 
 logger = get_logger(__name__)
 
 router = APIRouter()
+client = OpenRouterClient()
 
 @router.post("/chat")
 async def chat(request: PromptRequest):
     logger.info("/chat api route accessed")
 
     try:
-        result = chat_with_ai(request.prompt)
+        result = client.chat(request.prompt)
 
         if result is None:
             logger.error("Ai response failed in @post /chat")
--- app/services/openrouter_client.py
+++ app/services/openrouter_client.py
@@ -1,23 +1,29 @@
 import requests
 import json
-from app.core.config import OPENROUTER_KEY
+from app.core.config import OPENROUTER_KEY, OPENROUTER_URL
 from app.core.logger import get_logger
 
 logger = get_logger(__name__)
 
-def chat_with_ai(prompt: str):
 
-    logger.info("chat with ai initialized")
-    
-    url="https://openrouter.ai/api/v1/chat/completions"
-    
-    headers={
-            "Authorization": f"Bearer {OPENROUTER_KEY}",
-            "Content-Type": "application/json"
-    }
+model = "qwen/qwen3-30b-a3b:free"
 
-    payload = {
-        "model": "qwen/qwen3-30b-a3b:free", #currently hard coding free model for testing
+class OpenRouterClient:
+
+    def __init__(self, api_key: str = OPENROUTER_KEY):
+        self.api_key = api_key
+        self.url = OPENROUTER_URL
+    
+        self.headers = {
+            "Authorization":f"Bearer {OPENROUTER_KEY}",
+             "Content-Type": "application/json"
+        }
+
+    def chat(self, prompt : str) -> str | None :
+        logger.info("chat with ai initialized")
+
+        payload = {
+        "model": model, #currently hard coding free model for testing
         "messages":[
                 {
                     "role":"user",
@@ -25,22 +31,23 @@ def chat_with_ai(prompt: str):
                 }
             ],
     }
-
-
-    try:
-        response = requests.post(url, headers=headers, data=json.dumps(payload))
-        response.raise_for_status()
-        logger.info(f"AI response: {response.json()}")
-        return response.json()["choices"][0]["message"]["content"]
+        try:
+            response = requests.post(self.url, headers=self.headers, data=json.dumps(payload))
+            response.raise_for_status()
+            logger.info(f"AI response: {response.json()}")
+            return response.json()["choices"][0]["message"]["content"]
     
-    except requests.exceptions.HTTPError as e:
-        logger.error(f"HTTP error: {e} - Response: {response.text}")
+        except requests.exceptions.HTTPError as e:
+            logger.error(f"HTTP error: {e} - Response: {response.text}")
 
-    except requests.exceptions.RequestException as e:
-        logger.error(f"Reqeust failed: {e}")
+        except requests.exceptions.RequestException as e:
+            logger.error(f"Reqeust failed: {e}")
 
-    except KeyError:
-        logger.error(f"Unexpected response format: {response.text}")
+        except KeyError:
+            logger.error(f"Unexpected response format: {response.text}")
     
-    except Exception as e:
-        logger.exception(f"Unexpected error: {e}")
+        except Exception as e:
+            logger.exception(f"Unexpected error: {e}")
+        
+
+    
