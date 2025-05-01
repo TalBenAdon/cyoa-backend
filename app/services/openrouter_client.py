6682df3dcfe404c05d2bcad3e5b3f67ffcdbb2c1
Tal Benadon <talbenadon2@gmail.com>
2025-05-01 11:51:08 +0900

    fixed raised http exceptions in openrouter_client.py

--- app/services/openrouter_client.py
+++ app/services/openrouter_client.py
@@ -2,6 +2,7 @@ import requests
 import json
 from app.core.config import OPENROUTER_KEY, OPENROUTER_URL
 from app.core.logger import get_logger
+from fastapi import HTTPException
 
 logger = get_logger(__name__)
 
@@ -13,7 +14,7 @@ class OpenRouterClient:
     def __init__(self, api_key: str = OPENROUTER_KEY):
         self.api_key = api_key
         self.url = OPENROUTER_URL
-    
+
         self.headers = {
             "Authorization":f"Bearer {OPENROUTER_KEY}",
              "Content-Type": "application/json"
@@ -39,15 +40,19 @@ class OpenRouterClient:
     
         except requests.exceptions.HTTPError as e:
             logger.error(f"HTTP error: {e} - Response: {response.text}")
+            raise HTTPException(status_code=response.status_code, detail=response.text)
 
         except requests.exceptions.RequestException as e:
             logger.error(f"Reqeust failed: {e}")
+            raise HTTPException(status_code=502, detail="AI service could not be reached.")
 
         except KeyError:
             logger.error(f"Unexpected response format: {response.text}")
+            raise HTTPException(status_code=500, detail="Invalid AI response format")
     
         except Exception as e:
             logger.exception(f"Unexpected error: {e}")
+            raise HTTPException(status_code=500, detail="Internal server error")
         
 
     
