b7741e31318a2758e4b0506a9c494ae148740d0b
Tal Ben Adon <talbenadon2@gmail.com>
2025-05-13 11:47:01 +0900

    addited system message to include name tag with the first message

--- app/services/adventure.py
+++ app/services/adventure.py
@@ -10,6 +10,7 @@ class Adventure:
         self.scene_num = 0 #maybe add an additional "starting info" to let the ai generate a different type of start?
         # maybe change status to numbers? 0 is start, and the story progresses by numbers of "scenes" generated
         self.type = type
+        self.name = "" #adventure needs a short name for UI purposes
         self.history = [] #need to decide how to define adventure history
 
         self.current_story_text = None #current story text we just got from the ai (send to USER)
@@ -29,20 +30,27 @@ class Adventure:
             You are an experienced storyteller, like a D&D game master. You will adjust your storytelling style based on the type of adventure. 
             This adventure's type is: "{self.type}".
             
-         Every reply must be formatted like this:
-            <text>
-            Your adventure Narration here
-            </text>
-            <option1>
-            action the user may take
-            </option1>
-            <option2>
-            action the user may take
-            </option2>
-            <option3>
-            action the user may take
-            </option3>
-            """
+         Your responses must be formatted using the following tags:
+
+    - In the **first reply only**, include the title of the adventure using the <name> tag:
+        <name>
+        The adventure's name
+        </name>
+
+    - Every reply (including the first) must include the following:
+        <text>
+        Your adventure narration here
+        </text>
+        <option1>
+        Action the user may take
+        </option1>
+        <option2>
+        Action the user may take
+        </option2>
+        <option3>
+        Action the user may take
+        </option3>
+    """
             }
             
         
