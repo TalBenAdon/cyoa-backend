598ea02ae3eea2c27364cfbe2d34ddf5369dc49e
Tal Ben Adon <talbenadon2@gmail.com>
2025-06-15 15:42:51 +0900

    added AdventureSnapshot class to have an easier way to send a snapshot of the needed data to the user

---
+++ app/services/adventure_snapshot.py
@@ -0,0 +1,6 @@
+class AdventureSnapshot:
+    def __init__(self, adventure_id, type, scene_number, history ):
+        self.id = adventure_id
+        self.type = type
+        self.scene_number = scene_number
+        self.history = history
