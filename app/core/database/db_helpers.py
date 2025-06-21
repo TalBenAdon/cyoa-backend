5b2e895012ba89d4e87868b90c3d4ed173f18759
Tal Benadon <talbenadon2@gmail.com>
2025-06-21 14:06:37 +0900

    resolved 'pull adventures names and ids from db', now exceptions need to be handled at the top level

--- app/core/database/db_helpers.py
+++ app/core/database/db_helpers.py
@@ -29,7 +29,7 @@ logger = get_logger(__name__)
 #         logger.error(f"Error inserting adventure to database: {e}")
         
         
-def save_new_adventure(adventure: Adventure):
+def save_new_adventure(adventure: Adventure):  #TODO let the error bubble up to the routes, handle the exception over there.
     try:
         with db_cursor() as cursor:
             cursor.execute(
@@ -60,8 +60,7 @@ def save_new_adventure(adventure: Adventure):
         logger.error(f"Error saving adventure:{e}")
     
 
-def save_and_update_adventure(adventure: Adventure):
-    print(f"MY CHOSEN OPTION: : :{adventure.last_chosen_option}")
+def save_and_update_adventure(adventure: Adventure):  #TODO let the error bubble up to the routes, handle the exception over there.
     if not adventure.last_chosen_option:
         raise Exception("No last option within adventure") #TODO raise an exception for when theres no last_chosen_option
     try:
@@ -115,7 +114,7 @@ def save_and_update_adventure(adventure: Adventure):
 #         logger.error(f"Error inserting adventure history to database: {e}")
 
 
-def get_adventures_names_id_from_db():
+def get_adventures_names_id_from_db(): #TODO let the error bubble up to the routes, handle the exception over there.
     try:
         with db_cursor() as cursor:
            cursor.execute(
@@ -128,7 +127,7 @@ def get_adventures_names_id_from_db():
         logger.error(f"Error fetching adventures from database: {e}")
 
 
-def return_formatted_history(history_array_rows):
+def return_formatted_history(history_array_rows):  #TODO let the error bubble up to the routes, handle the exception over there.
     formatted_history = []
     for row in history_array_rows:
         try:
@@ -142,7 +141,7 @@ def return_formatted_history(history_array_rows):
             logger.error(f"Error formatting history entries: {e}")
     return formatted_history
 
-        
+         #TODO let the error bubble up to the routes, handle the exception over there.
 def get_adventure_by_id(adventure_id :str):  #TODO as the app progresses check if required by itself
     try:
         with db_cursor() as cursor:
@@ -157,7 +156,7 @@ def get_adventure_by_id(adventure_id :str):  #TODO as the app progresses check i
          logger.error(f"Error fetching adventure from database: {e}")
              
          
-def get_adventure_with_history_by_id(adventure_id):
+def get_adventure_with_history_by_id(adventure_id): #TODO let the error bubble up to the routes, handle the exception over there.
     try:
         with db_cursor() as cursor:
             cursor.execute(
@@ -186,6 +185,7 @@ def get_adventure_with_history_by_id(adventure_id):
         return None #TODO can raise a different error later 
          
          
+          #TODO let the error bubble up to the routes, handle the exception over there.
 def get_adventure_history_by_id(adventure_id: str): #TODO as the app progresses check if required by itself
     try:
         with db_cursor() as cursor:
